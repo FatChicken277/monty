621b15bf03d031ff854bfd55ba99d79cc31c531d
FatChicken277 <1637@holbertonschool.com>
2020-05-12 14:34:43 -0500

    added comments and prototypes

--- monty.h
+++ monty.h
@@ -1,23 +1,12 @@
 #ifndef _HOLBERTON_
 #define _HOLBERTON_
 
+/** ====Libraries==== **/
+
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct stack_s
-{
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
+
+/** ====Structures==== **/
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -30,8 +19,27 @@ typedef struct stack_s
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/** ====Prototypes==== **/
+
+stack_t *create_node(int n);
+
 #endif
