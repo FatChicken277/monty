fd2e57a1e2c820ea0ae843ebb16c07a64dbabe57
Diego Gómez <dagomez2530@gmail.com>
2020-05-13 00:56:26 +0000

    Fixing _strtok

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "monty.h"
-void _strtok(char *line);
-void push(char *token);
+void _strtok(char *line, unsigned int count __attribute__((unused)));
+/*void push(char *token);*/
 
 int main(int argc, char *argv[])
 {
 	char *filename = argv[1], *line = NULL;
-	int count = 1;
+	unsigned int count = 1;
 	FILE *file;
 	size_t len = 0;
 
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	while ((getline(&line, &len, file)) != -1)
 	{
-		_strtok(line);
+		_strtok(line, count);
 		count++;
 	}
 
@@ -34,33 +34,39 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
-void _strtok(char *line)
+void _strtok(char *line, unsigned int count __attribute__((unused)))
 {
+	int i = 0;
 	char *token = NULL;
+	instruction_t finder[] = {
+		{"push", NULL},
+		{"pall", NULL},
+		{"pint", NULL},
+		{"pop", NULL},
+		{"swap", NULL},
+		{"add", NULL},
+		{"nop", NULL}
+	};
 
 	token = strtok(line, " \n\t\r\v\f\a");
-
+	printf("token %s\n", token);
 	if (token)
 	{
-		if (strcmp(token, "push") == 0)
+		if (strcmp(token, finder[0].opcode) == 0)
 		{
 			token = strtok(NULL, " \n\t\r\v\f\a");
-			push(token);
-		}
-	}
-}
-
-void push(char *token)
-{
-	int i;
-
-	for (i = 0; token[i] != '\0'; i++)
-	{
-		if (token[i] < '0' || token[i] > '9')
-		{
-			printf("error\n");
+			printf("%s\n", token);
 			return;
 		}
+
+		for (i = 1; i < 7; i++)
+		{
+			if (strcmp(token, finder[i].opcode) == 0)
+			{
+				/*finder[i].f(*stack, cont);*/
+				return;
+			}
+		}
+		/*Imprimir error en caso de que no se encuentre el opcode*/
 	}
-	printf("%d\n", atoi(token));
 }
