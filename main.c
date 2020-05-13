894ea7fae59e218cf80ac2147b32d96347ff7ef7
FatChicken277 <1637@holbertonschool.com>
2020-05-13 14:31:48 -0500

    fixed betty and remove prototype

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "monty.h"
-void _strtok(char *line, unsigned int count, stack_t **stack);
 /*void push(char *token);*/
 
+/**
+ * main - read and run monty files.
+ * @argv: arguments.
+ * @argc: number of arguments.
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	char *filename = argv[1], *line = NULL;
@@ -35,6 +40,12 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+ * _strtok - delete all unnecessary stuff.
+ * @stack: head.
+ * @count: line_number.
+ * @line: one line of buffer.
+ */
 void _strtok(char *line, unsigned int count, stack_t **stack)
 {
 	int i = 0;
@@ -42,11 +53,11 @@ void _strtok(char *line, unsigned int count, stack_t **stack)
 	instruction_t finder[] = {
 		{"push", _push},
 		{"pop", _pop},
-		{"pint", NULL},
-		{"pall", NULL},
-		{"swap", NULL},
-		{"add", NULL},
-		{"nop", NULL}};
+		{"pint", _pint},
+		{"pall", _pall},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop}};
 
 	token = strtok(line, " \n\t\r\v\f\a");
 	printf("token %s\n", token);
