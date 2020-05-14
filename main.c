fb82f44ab6f273b07ee7b7a7be8e1505eb8e09d0
Diego Gómez <dagomez2530@gmail.com>
2020-05-14 16:43:36 +0000

    Adding _pchar to the main program

--- main.c
+++ main.c
@@ -66,7 +66,7 @@ void _strtok(char *line, unsigned int count, stack_t **stack, FILE *file)
 	instruction_t finder[] = {
 		{"push", _push}, {"pop", _pop}, {"pint", _pint}, {"pall", _pall},
 		{"swap", _swap}, {"add", _add}, {"nop", _nop}, {"sub", _sub},
-		{"mul", _mul}, {"div", _div}, {"mod", _mod}};
+		{"mul", _mul}, {"div", _div}, {"mod", _mod}, {"pchar", _pchar}};
 
 	token = strtok(line, " \n\t\r\v\f\a");
 	gnum = "";
@@ -82,7 +82,7 @@ void _strtok(char *line, unsigned int count, stack_t **stack, FILE *file)
 			check_error(&(*stack), line, file);
 			return;
 		}
-		for (i = 1; i < 12; i++)
+		for (i = 1; i < 13; i++)
 		{
 			if (strcmp(token, finder[i].opcode) == 0)
 			{
